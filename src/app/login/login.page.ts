9a8ba3f1d0d69dc27899b63feab5f74610c63d3b
e5558005 <ravi.bhisetti@fisglobal.com>
2019-03-16 18:59:01 +0530

    navigating to home screen on click of login button

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'login-page',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
 })
 
 export class LoginPage {
+    constructor(private router: Router) {
+    }
 
+    navigateToHome() {
+        this.router.navigateByUrl('/home');
+    }
 }
