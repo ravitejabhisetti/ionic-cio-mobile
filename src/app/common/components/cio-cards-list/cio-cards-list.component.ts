2dd89f39a11bbc6fafa76ea501bc2eb3188c9720
e5558005 <ravi.bhisetti@fisglobal.com>
2019-05-04 12:40:33 +0530

    changed the way of dispalying css

--- src/app/common/components/cio-cards-list/cio-cards-list.component.ts
+++ src/app/common/components/cio-cards-list/cio-cards-list.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'cio-cards-list',
-    templateUrl: 'cio-cards-list.component.html'
+    templateUrl: 'cio-cards-list.component.html',
+    styleUrls: ['cio-cards-list.component.scss']
 })
 
 export class CioCardsListComponent {
